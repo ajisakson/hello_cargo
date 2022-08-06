260bddfcfb5049af5077b27570707e2506494019
Austin Isakson <austini@crowdhubapps.com>
2022-08-05 19:57:29 -0700

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Use `cargo new` to instantiate a nice clean Rust project");
+    println!("Use `cargo build` to compile");
+    println!("Use `cargo run` to compile and run the executable");
+    println!("Use `cargo check ` to ensure there are no compilation errors without building");
 }
